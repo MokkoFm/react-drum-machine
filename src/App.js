dbc6fae85fcb30c5fb943f12da57ac6dd96850ee
MokkoFm <mokkofmpoetry@gmail.com>
2021-02-11 15:00:16 +0200

    add current sound as a text at the display

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 
 const sounds = [
-  { id: 'Q', letter: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
-  { id: 'W', letter: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
-  { id: 'E', letter: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
-  { id: 'A', letter: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
-  { id: 'S', letter: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
-  { id: 'D', letter: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
-  { id: 'Z', letter: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
-  { id: 'X', letter: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
-  { id: 'C', letter: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'  },
+  { id: 'Q', letter: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', sound: "Heater 1", },
+  { id: 'W', letter: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', sound: "Heater 2" },
+  { id: 'E', letter: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', sound: "Heater 3" },
+  { id: 'A', letter: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', sound: "Heater 4" },
+  { id: 'S', letter: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', sound: "Clap"},
+  { id: 'D', letter: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', sound: "Open HH" },
+  { id: 'Z', letter: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', sound: "Kick n' Hat" },
+  { id: 'X', letter: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', sound: "Kick" },
+  { id: 'C', letter: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', sound: "Closed HH"  },
 ]
 
 
@@ -19,38 +19,36 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playing: false,
+      playing: 'Heater 1',
     }
     this.pushPad = this.pushPad.bind(this)
   }
+  
   pushPad(e) {
-    let audio = new Audio(e.target.children[0].currentSrc)
-    audio.play()
+    debugger
+    const audio = e.target.children[0];
+    audio.play();
     this.setState({
-      playing: true,
+      playing: e.target.id,
     })
-    setTimeout(() => {
-      this.setState({
-        playing: false,
-      })
-    }, 500)
   }
 
   render() {
-    const pads = sounds.map(pad => 
-    <div className="drum-pad" id={pad.id} onClick={this.pushPad}>
+    const pads = sounds.map((pad, index) => 
+    <div key={index} className="drum-pad" id={pad.sound} onClick={this.pushPad}>
+      <audio key={index} className="clip" id={pad.id} src={pad.src}></audio>
       {pad.letter}
-      <audio src={pad.src} className="clip" id={pad.id}></audio>
     </div>)
     return (
       <div className="App">
         <div id="drum-machine">
-          <div id="display">
+          <div id="pads">
             {pads}
           </div>
         </div>
-        <div>
+        <div id="display">
           <h1 className="title">Drum Machine</h1>
+          <p className="sound_name">{this.state.playing}</p>
         </div>
       </div>
     )
